ca382407f2eef9022e005023523c15c7bf451c8c
RamPratheek28 <rampratheeks28@gmail.com>
2023-11-02 15:31:36 +0530

    first

---
+++ Reports/Html_Report/report.js
@@ -0,0 +1,364 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("adactin.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Hotel Booking In Adactin Application",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "User Login In The Web Application",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application;user-login-in-the-web-application",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@smokeTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "user Launch The Url In Adactin Application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "user Enter The Username In Username Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user Enter The Password In Password Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "user Click The Login button and It Navigates To Search Hotel Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefinition.user_Launch_The_Url_In_Adactin_Application()"
+});
+formatter.result({
+  "duration": 11558686200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Enter_The_Username_In_Username_Field()"
+});
+formatter.result({
+  "duration": 2743014200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Enter_The_Password_In_Password_Field()"
+});
+formatter.result({
+  "duration": 2616008200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Click_The_Login_button_and_It_Navigates_To_Search_Hotel_Page()"
+});
+formatter.result({
+  "duration": 3214193500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "User Search Hotel In The Web Application",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application;user-search-hotel-in-the-web-application",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 10,
+      "name": "@smokeTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 12,
+  "name": "user Select The Locations In The Dropdown Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "user Select The Hotels In The Dropdown Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "user Select The Roomtype In The Dropdown Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "user Select The Number of Rooms In the Dropdown Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 16,
+  "name": "user Enter The Date Check In Date Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 17,
+  "name": "user Enter The Date Check Out Date Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "user Select The Adults Per Room In The Required Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "user Select The Children Per Room In The Required Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 20,
+  "name": "user Click The Search button and It Navigates To Select Hotel Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefinition.user_Select_The_Locations_In_The_Dropdown_Field()"
+});
+formatter.result({
+  "duration": 1376575000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Select_The_Hotels_In_The_Dropdown_Field()"
+});
+formatter.result({
+  "duration": 481796600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Select_The_Roomtype_In_The_Dropdown_Field()"
+});
+formatter.result({
+  "duration": 193069000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Select_The_Number_of_Rooms_In_the_Dropdown_Field()"
+});
+formatter.result({
+  "duration": 155049500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Enter_The_Date_Check_In_Date_Field()"
+});
+formatter.result({
+  "duration": 482680100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Enter_The_Date_Check_Out_Date_Field()"
+});
+formatter.result({
+  "duration": 441034500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Select_The_Adults_Per_Room_In_The_Required_Field()"
+});
+formatter.result({
+  "duration": 138456600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Select_The_Children_Per_Room_In_The_Required_Field()"
+});
+formatter.result({
+  "duration": 138208800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Click_The_Search_button_and_It_Navigates_To_Select_Hotel_Page()"
+});
+formatter.result({
+  "duration": 2224563700,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 23,
+  "name": "User Select Hotel In The Web Application",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application;user-select-hotel-in-the-web-application",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 22,
+      "name": "@smokeTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 24,
+  "name": "user Select The Hotel By Clicking Select button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 25,
+  "name": "user Click The Continue button and It Navigates To Hotel Booking Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefinition.user_Select_The_Hotel_By_Clicking_Select_button()"
+});
+formatter.result({
+  "duration": 360825900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Click_The_Continue_button_and_It_Navigates_To_Hotel_Booking_Page()"
+});
+formatter.result({
+  "duration": 2077715700,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 28,
+  "name": "User Book Hotel In The Web Application",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application;user-book-hotel-in-the-web-application",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 27,
+      "name": "@smokeTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 29,
+  "name": "user Enter The First Name In The Username Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 30,
+  "name": "user Enter The Last Name In The Lastname Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "user Enter The Billing Address In The Required Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "user Enter The Sixteen Digit Credit Card Number In The Required Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 33,
+  "name": "user Select The Credit Card Type In The Dropdown Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 34,
+  "name": "user Select The Expiry Date and Expiry Year In The Dropdown Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 35,
+  "name": "user Enter The CVV Number",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 36,
+  "name": "User Click The Booknow and It Navigates To The Booking Confirmation Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefinition.user_Enter_The_First_Name_In_The_Username_Field()"
+});
+formatter.result({
+  "duration": 136057500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Enter_The_Last_Name_In_The_Lastname_Field()"
+});
+formatter.result({
+  "duration": 214691200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Enter_The_Billing_Address_In_The_Required_Field()"
+});
+formatter.result({
+  "duration": 153175500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Enter_The_Sixteen_Digit_Credit_Card_Number_In_The_Required_Field()"
+});
+formatter.result({
+  "duration": 433699600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Select_The_Credit_Card_Type_In_The_Dropdown_Field()"
+});
+formatter.result({
+  "duration": 178588500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Select_The_Expiry_Date_and_Expiry_Year_In_The_Dropdown_Field()"
+});
+formatter.result({
+  "duration": 322379900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Enter_The_CVV_Number()"
+});
+formatter.result({
+  "duration": 133003000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition.user_Click_The_Booknow_and_It_Navigates_To_The_Booking_Confirmation_Page()"
+});
+formatter.result({
+  "duration": 267544400,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 39,
+  "name": "User Confirms Booking Hotel In The Web Application",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application;user-confirms-booking-hotel-in-the-web-application",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 38,
+      "name": "@smokeTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 40,
+  "name": "user Click My Iternary and It Navigates To The Booked Iternary Page",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
